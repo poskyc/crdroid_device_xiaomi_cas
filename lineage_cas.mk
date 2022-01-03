6f412e91ad49944e8abd25a835d9dfa9332dda92
poskyc <poskyc@outlook.com>
2022-01-03 11:34:20 +0800

    crdroid ify

--- lineage_cas.mk
+++ lineage_cas.mk
@@ -6,8 +6,7 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from the device configuration.
 $(call inherit-product, device/xiaomi/cas/device.mk)
@@ -15,7 +14,7 @@ $(call inherit-product, device/xiaomi/cas/device.mk)
 # Inherit from the Aosp configuration.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := aosp_cas
+PRODUCT_NAME := lineage_cas
 PRODUCT_DEVICE := cas
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := M2007J1SC
@@ -25,4 +24,4 @@ DEVICE_MAINTAINER := poskyc
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := Xiaomi/cas/cas:12/SKQ1.211006.001/21.12.8:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/cas/cas:12/RKQ1.211001.001/21.12.30:user/release-keys
