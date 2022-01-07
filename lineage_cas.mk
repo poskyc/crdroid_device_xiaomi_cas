83a8c7297fdab29f59afce6fab5086dc167d48bb
poskyc <poskyc@outlook.com>
2022-01-07 22:32:06 +0800

    fix build error

--- lineage_cas.mk
+++ lineage_cas.mk
@@ -6,13 +6,13 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from the device configuration.
 $(call inherit-product, device/xiaomi/cas/device.mk)
 
 # Inherit from the Aosp configuration.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_cas
 PRODUCT_DEVICE := cas
